3959015179131f1b0bd3d3f03b47df5b71b78f43
Brayan Baquero <babaquero3@misena.edu.co>
2022-02-16 17:21:03 -0500

    Modelos y rutas funcionando

--- controllers/admins.js
+++ controllers/admins.js
@@ -1,13 +1,13 @@
-const adminRouter = require('express').Router();
-const Admin = require('../models/admin');
+const adminRouter = require("express").Router();
+const Admin = require("../models/admin");
 
-adminRouter.get('/', async (req, res) => {
-  const admins = await Admin.find({}).populate('Agents');
+adminRouter.get("/", async (req, res) => {
+  const admins = await Admin.find({});
 
   res.json(admins);
 });
 
-adminRouter.post('/', async (req, res) => {
+adminRouter.post("/", async (req, res) => {
   const admin = new Admin(req.body);
 
   const savedAdmin = await admin.save();
