00550c97f8f0d3cee3f67c92ddf1f43f66cf32ce
Dairo01001 <garcianaranjodairo@gmail.com>
2022-02-17 08:21:50 -0500

    Admin Routes

--- controllers/admins.js
+++ controllers/admins.js
@@ -1,17 +1,46 @@
-const adminRouter = require('express').Router();
-const Admin = require('../models/admin');
+const adminRouter = require("express").Router();
+const Admin = require("../models/admin");
 
-adminRouter.get('/', async (req, res) => {
-  const admins = await Admin.find({}).populate('agents');
+adminRouter.get("/", async (req, res) => {
+  const admins = await Admin.find({});
 
   res.json(admins);
 });
 
-adminRouter.post('/', async (req, res) => {
+adminRouter.post("/", async (req, res) => {
   const admin = new Admin(req.body);
 
   const savedAdmin = await admin.save();
   res.status(201).json(savedAdmin);
+});
+
+adminRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const admin = Admin.findById(id).populate("agents");
+
+  if (admin) return res.json(admin);
+  res.status(404).end();
+});
+
+adminRouter.put("/:id", async (req, res) => {
+  const { id, ...newAdminInfo } = req.body;
+
+  const admin = {
+    ...newAdminInfo,
+  };
+
+  const updatedAdmin = await Property.findByIdAndUpdate(id, admin, {
+    new: true,
+  });
+  res.json(updatedAdmin);
+});
+
+adminRouter.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  await Property.findByIdAndRemove(id);
+  res.status(204).end();
 });
 
 module.exports = adminRouter;
