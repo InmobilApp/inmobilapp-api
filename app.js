bfbdac4765442c0a681a900c4e4c1b78c93e0bff
Dairo01001 <garcianaranjodairo@gmail.com>
2022-02-23 23:10:10 -0500

    All origin nodemon dep

--- app.js
+++ app.js
@@ -28,6 +28,16 @@ app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
 app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 app.set('port', PORT);
 
 app.use('/api/admins', adminRouter);
