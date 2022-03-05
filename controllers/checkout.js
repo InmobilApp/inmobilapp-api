2ba0c7da97802d0051a9be38f534545bbb79419d
David De la Hoz <dricardelo@gmail.com>
2022-03-05 02:02:16 -0500

    mercado pago backend

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -1,45 +1,43 @@
-const checkoutRouter = require('express').Router()
-const mercadoP = require('mercadopago');
+const checkoutRouter = require("express").Router();
+const mercadopago = require("mercadopago");
 
-mercadoP.configure({
-    access_token: "TEST-3654398483251205-030418-77f3aadc4ce3f0ab0371280938803852-460318749",
-});
+const createorder = async (req, res) => {
+  //TEST-3793295535523231-030506-300abfd4672588102dc3dca173408d87-1084485093
+  mercadopago.configure({
+    access_token:
+      "TEST-3793295535523231-030506-300abfd4672588102dc3dca173408d87-1084485093",
+  });
 
-
-
-checkoutRouter.post('/', async (req, res)=>{
-  const {title, unit_price, quantity} = req.body
-  let preference = {
+  var preference = {
     items: [
-      { 
-        title: 'My Item',
-        unit_price: 1,
+      {
+        title: "Pelota",
         quantity: 1,
-        currency_id:'ARS'
-      }
+        currency_id: "COP",
+        unit_price: 20000,
+      },
     ],
-    notification_url: "http://localhost:3001/checkout/notifications",
-    /* back_urls: {
-        "success": "http://localhost:3000/checked",
-        "failure": "http://localhost:3000/checked",
-        "pending": "http://localhost:3000/checked"
-    }, */
-    /* auto_return: "approved", */
+    notification_url: "http://localhost:3001/api/notification",
   };
-      
-  mercadoP.preferences.create(preference)
-    .then(function(response){
-      return res.json(response)
-      /* global.id = response.body.id; */
-    }).catch(function(error){
-      console.log(error);
+
+  mercadopago.preferences
+    .create(preference)
+    .then((r) => {
+      return res.json(r);
+    })
+    .catch((err) => {
+      console.log(err);
     });
-});
+};
 
-checkoutRouter.post("/notification", (req, res) => {
+const notification = async (req, res) => {
   const data = req.query;
-  console.log(data)
-  res.status(200).send('notificación');
-})
 
-module.exports = checkoutRouter
+  console.log(data);
+  res.status(200);
+};
+
+checkoutRouter.post("/", createorder);
+checkoutRouter.post("/notification", notification);
+
+module.exports = checkoutRouter;
