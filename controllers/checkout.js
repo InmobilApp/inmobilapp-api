2dbd229b087e74c422e0324588ac7e8a5e0507ac
David De la Hoz <dricardelo@gmail.com>
2022-03-06 21:59:38 -0500

    mercado pago integrado

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -3,7 +3,9 @@ const mercadopago = require("mercadopago");
 
 const createorder = async (req, res) => {
   //TEST-3793295535523231-030506-300abfd4672588102dc3dca173408d87-1084485093
+  const { title, price } = req.body;
   mercadopago.configure({
+    public_key: "TEST-0c8b4a8a-9e63-436c-960b-b2e75a65eee4",
     access_token:
       "TEST-3793295535523231-030506-300abfd4672588102dc3dca173408d87-1084485093",
   });
@@ -11,10 +13,10 @@ const createorder = async (req, res) => {
   var preference = {
     items: [
       {
-        title: "Pelota",
+        title: title,
         quantity: 1,
         currency_id: "COP",
-        unit_price: 20000,
+        unit_price: price,
       },
     ],
     notification_url: "http://localhost:3001/api/notification",
@@ -23,7 +25,8 @@ const createorder = async (req, res) => {
   mercadopago.preferences
     .create(preference)
     .then((r) => {
-      return res.json(r);
+      console.log(r);
+      return res.send(r.body.id);
     })
     .catch((err) => {
       console.log(err);
